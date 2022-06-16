f7822fe166e5333f6f1a2485df54c85083b0e709
Jake Sonn <jakesonn321@gmail.com>
2022-06-16 17:55:19 -0400

    Hello

--- external.js
+++ external.js
@@ -1,1 +1,19 @@
-console.log("hello puss")
+// 1 is rock
+//2 is sicissors
+//3 is paper
+
+
+
+function getComputer(max) {
+    return Math.floor(Math.random() * max);
+}
+
+var compResult;
+compResult = getComputer("3")
+//console.log(compResult);
+
+
+
+
+
+
